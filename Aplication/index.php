ce71051b449c2781a82a7c03e3d30bdba96a6823
IvanMiljkovicPMF <108157316+IvanMiljkovicPMF@users.noreply.github.com>
2022-10-26 21:46:20 +0200

    Update index.php

--- Aplication/index.php
+++ Aplication/index.php
@@ -22,8 +22,8 @@
     if(isset($_GET["dodajp"]))
     {
         $ki=$_GET["ime"];
-         $s=$_GET["sifra"];
-         $db->dodajProdavca($ki, $s);
+        $s=$_GET["sifra"];
+        $db->dodajProdavca($ki, $s);
     }
 ?>
 <html>
@@ -64,7 +64,7 @@
         </div><br>
         <form id="forma2">
         Korisnicko ime:<input type="text" name="ime"><br>
-       Sifra: <input type="text" name="sifra"><br>
+        Sifra: <input type="text" name="sifra"><br>
         
        <input type="submit"  name="dodajp" value="dodajProdavca"><br>
         </form>
@@ -82,75 +82,74 @@
         function proizvodi()
         {
             
-        admin=<?php echo $_SESSION["admin"];?>;
-        http = new XMLHttpRequest();
-        http.onreadystatechange = function() {
-        if (http.readyState == 4 && http.status == 200) {
-        proizvodi = JSON.parse(http.responseText);
-        tabela = "<tr><th>Naziv</th><th>Tip</th><th>Opis</th><th>Kolicina</th></tr>";
-        for(i = 0; i < proizvodi.length; i++){
-            
-            
-            if(admin==1){
-             tabela+="<tr><td>"+proizvodi[i].naziv+"</td><td>"+proizvodi[i].tip+"</td><td>"+proizvodi[i].opis+"</td><td>"+proizvodi[i].kolicina+"</td>";   
-             tabela+="<td><input type='button' onclick='obrisiProizvod("+proizvodi[i].id+")' value='Obrisi'></td>";
-            }
-            else
-            {
-                if(proizvodi[i].kolicina!=0)
-                {
-                    tabela+="<tr><td>"+proizvodi[i].naziv+"</td><td>"+proizvodi[i].tip+"</td><td>"+proizvodi[i].opis+"</td><td>"+proizvodi[i].kolicina+"</td>"
-                     tabela+="<td><input type='button' onclick='naruciProizvod("+proizvodi[i].id+")' value='Naruci'></td>";
-                }
+            admin=<?php echo $_SESSION["admin"];?>;
+            http = new XMLHttpRequest();
+            http.onreadystatechange = function() {
                 
-            }
-            
-            tabela+="</tr>";
-        }
+                if (http.readyState == 4 && http.status == 200) {
+                    proizvodi = JSON.parse(http.responseText);
+                    tabela = "<tr><th>Naziv</th><th>Tip</th><th>Opis</th><th>Kolicina</th></tr>";
+                    for(i = 0; i < proizvodi.length; i++){
+                        if(admin==1){
+                            tabela+="<tr><td>"+proizvodi[i].naziv+"</td><td>"+proizvodi[i].tip+"</td><td>"+proizvodi[i].opis+"</td><td>"+proizvodi[i].kolicina+"</td>";   
+                            tabela+="<td><input type='button' onclick='obrisiProizvod("+proizvodi[i].id+")' value='Obrisi'></td>";
+                        }
+                       else
+                        {
+                            if(proizvodi[i].kolicina!=0)
+                            {
+                                tabela+="<tr><td>"+proizvodi[i].naziv+"</td><td>"+proizvodi[i].tip+"</td><td>"+proizvodi[i].opis+"</td><td>"+proizvodi[i].kolicina+"</td>"
+                                tabela+="<td><input type='button' onclick='naruciProizvod("+proizvodi[i].id+")' value='Naruci'></td>";
+                            }
+
+                        }
+
+                        tabela+="</tr>";
+                     }
         
-        document.getElementById("tabela1").innerHTML=tabela;
-    }
-    };
-        http.open("GET", "service.php?showTable" ,true);
-        http.send();
-    }
+                    document.getElementById("tabela1").innerHTML=tabela;
+                }
+            };
+            http.open("GET", "service.php?showTable" ,true);
+            http.send();
+        }
     
     
       function obrisiProizvod(id)
       {
-          http = new XMLHttpRequest();
-        http.onreadystatechange = function() {
-        if (http.readyState == 4 && http.status == 200) {
+            http = new XMLHttpRequest();
+            http.onreadystatechange = function() {
+            if (http.readyState == 4 && http.status == 200) {
             window.location="index.php";
-        }
-    };
-     http.open("GET", "service.php?obrisi&id="+id ,true);
-        http.send();
+            }
+        };
+         http.open("GET", "service.php?obrisi&id="+id ,true);
+         http.send();
       }
       
       
       function naruciProizvod(id)
       {
-          http = new XMLHttpRequest();
-        http.onreadystatechange = function() {
-        if (http.readyState == 4 && http.status == 200) {
-            window.location="index.php";
-        }
-    };
-     http.open("GET", "service.php?update&id1="+id ,true);
-        http.send();
+            http = new XMLHttpRequest();
+            http.onreadystatechange = function() {
+            if (http.readyState == 4 && http.status == 200) {
+                window.location="index.php";
+            }
+          };
+            http.open("GET", "service.php?update&id1="+id ,true);
+            http.send();
       }
       
       
       function forma1()
       {
-          http = new XMLHttpRequest();
-        http.onreadystatechange = function() {
-        if (http.readyState == 4 && http.status == 200) {
-            window.location="index.php";
-        }
-    };
-     http.open("GET", "service.php?dodajproizvod&id1="+id ,true);
+            http = new XMLHttpRequest();
+            http.onreadystatechange = function() {
+                if (http.readyState == 4 && http.status == 200) {
+                    window.location="index.php";
+                    }
+            };
+        http.open("GET", "service.php?dodajproizvod&id1="+id ,true);
         http.send();
       }
       
