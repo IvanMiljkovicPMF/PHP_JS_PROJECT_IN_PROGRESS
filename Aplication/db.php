ef30dcb55d7e9dd5116430e3d8d58e35da7a56f3
IvanMiljkovicPMF <108157316+IvanMiljkovicPMF@users.noreply.github.com>
2022-10-26 21:40:13 +0200

    Update db.php

--- Aplication/db.php
+++ Aplication/db.php
@@ -32,17 +32,20 @@
         const pass = ''; 
         const dbname = "magacin"; 
         private $dbh;
-        function __construct() 
-        { try 
+            function __construct() 
             { 
-            $string="mysql:host=".self::host.";dbname=".self::dbname; 
-            $this->dbh = new PDO($string, self::user, self::pass); 
-            
-            } catch(PDOException $e) 
-            { 
-                echo "GRESKA: "; 
-                echo $e->getMessage(); }
+                try 
+                { 
+                    $string="mysql:host=".self::host.";dbname=".self::dbname; 
+                    $this->dbh = new PDO($string, self::user, self::pass); 
+
                 } 
+                catch(PDOException $e) 
+                { 
+                    echo "GRESKA: "; 
+                    echo $e->getMessage(); 
+                }
+            } 
             function __destruct() 
             { 
                 $this->dbh = null; 
@@ -50,23 +53,31 @@
             }
             public function checkUser($user,$pass)
             {
-                try { $sql = "SELECT * FROM korisnik WHERE korisnicko_ime='$user' and sifra='$pass'"; 
-                $pdo_izraz = $this->dbh->query($sql);
-                $obj = $pdo_izraz->fetch(PDO::FETCH_ASSOC); 
-                return $obj; } 
+                try { 
+                    $sql = "SELECT * FROM korisnik WHERE korisnicko_ime='$user' and sifra='$pass'"; 
+                    $pdo_izraz = $this->dbh->query($sql);
+                    $obj = $pdo_izraz->fetch(PDO::FETCH_ASSOC); 
+                    return $obj; 
+                } 
                 catch(PDOException $e) 
-                { echo "GRESKA: "; 
-                echo $e->getMessage(); }
+                { 
+                    echo "GRESKA: "; 
+                    echo $e->getMessage(); 
+                }
             }
             public function getUloga($id)
             {
-                try { $sql = "SELECT * FROM tip_korisnika WHERE id=$id"; 
-                $pdo_izraz = $this->dbh->query($sql);
-                $obj = $pdo_izraz->fetch(PDO::FETCH_ASSOC); 
-                return $obj; } 
+                try { 
+                    $sql = "SELECT * FROM tip_korisnika WHERE id=$id"; 
+                    $pdo_izraz = $this->dbh->query($sql);
+                    $obj = $pdo_izraz->fetch(PDO::FETCH_ASSOC); 
+                    return $obj; 
+                    } 
                 catch(PDOException $e) 
-                { echo "GRESKA: "; 
-                echo $e->getMessage(); }
+                { 
+                    echo "GRESKA: "; 
+                    echo $e->getMessage(); 
+                }
             }
             public function getProizvodeID($id)
             {
@@ -107,77 +118,100 @@
                 echo json_encode($proizvod);
                 } 
                 catch(PDOException $e) 
-                { echo "GRESKA: "; 
-                echo $e->getMessage(); }
+                { 
+                    echo "GRESKA: "; 
+                    echo $e->getMessage(); 
+                }
             }
             public function brisiProizvode($id) 
-                    { try 
-                { $sql = "DELETE FROM proizvod WHERE id=$id";
-                $pdo_izraz = $this->dbh->exec($sql);
-                return true; } 
-            catch(PDOException $e) 
-            { echo "GRESKA: ";
-            echo $e->getMessage(); 
-            return false; } }
+            { 
+                try 
+                { 
+                    $sql = "DELETE FROM proizvod WHERE id=$id";
+                    $pdo_izraz = $this->dbh->exec($sql);
+                    return true; 
+                } 
+                catch(PDOException $e) 
+                {   
+                    echo "GRESKA: ";
+                    echo $e->getMessage(); 
+                    return false; 
+                } 
+            }
             
             public function dodajProizvod($n, $t,$o,$k) 
-            { try 
+            { 
+                try 
                 {
-                $sql = "SELECT * FROM proizvod WHERE naziv='$n' and opis='$o'"; 
-                $pdo_izraz = $this->dbh->query($sql);
-                $obj = $pdo_izraz->fetch(PDO::FETCH_ASSOC); 
+                    $sql = "SELECT * FROM proizvod WHERE naziv='$n' and opis='$o'"; 
+                    $pdo_izraz = $this->dbh->query($sql);
+                    $obj = $pdo_izraz->fetch(PDO::FETCH_ASSOC); 
                 
-                if($obj)
-                {
-                    $id=$obj["id"];
-                    $sql = "UPDATE proizvod SET kolicina=:kolicina WHERE id=:id"; 
-                    $pdo_izraz = $this->dbh->prepare($sql); 
-                    $kolicina1= $obj["kolicina"]+$k;
-                    $pdo_izraz->bindParam(':id', $id); 
-                    $pdo_izraz->bindParam(':kolicina', $kolicina1); 
-                    $pdo_izraz->execute(); 
-                    return true;
-                }
-                else
-                {
-                  $sql = "INSERT INTO proizvod(naziv,tip,opis,kolicina) VALUES ('$n', '$t', '$o',$k)"; 
-                $pdo_izraz = $this->dbh->exec($sql);
-                return true;  
-                }
+                    if($obj)
+                    {
+                        $id=$obj["id"];
+                        $sql = "UPDATE proizvod SET kolicina=:kolicina WHERE id=:id"; 
+                        $pdo_izraz = $this->dbh->prepare($sql); 
+                        $kolicina1= $obj["kolicina"]+$k;
+                        $pdo_izraz->bindParam(':id', $id); 
+                        $pdo_izraz->bindParam(':kolicina', $kolicina1); 
+                        $pdo_izraz->execute(); 
+                        return true;
+                    }
+                    else
+                    {
+                        $sql = "INSERT INTO proizvod(naziv,tip,opis,kolicina) VALUES ('$n', '$t', '$o',$k)"; 
+                        $pdo_izraz = $this->dbh->exec($sql);
+                        return true;  
+                    }
                  } 
                 catch(PDOException $e) 
-                { echo "GRESKA: "; 
-                echo $e->getMessage(); 
-                return false; } }
-            public function dodajProdavca($ki, $s) 
-            { try 
-                { $sql = "SELECT * FROM korisnik WHERE korisnicko_ime='$ki' and sifra='$s' "; 
-                $pdo_izraz = $this->dbh->query($sql);
-                $obj = $pdo_izraz->fetch(PDO::FETCH_ASSOC); 
-                if($obj)
-                {
-                    return false;
-                }
-                else{
-                    $sql = "INSERT INTO korisnik(korisnicko_ime,sifra,uloga) VALUES ('$ki', '$s',1)"; 
-                $pdo_izraz = $this->dbh->exec($sql); return true;
-                }
-                 } 
-                catch(PDOException $e) 
-                { echo "GRESKA: "; 
-                echo $e->getMessage(); return false; } }
-            public function izmeniKolicinu($id) 
-                    { try { $sql = "UPDATE proizvod SET kolicina=:kolicina WHERE id=:id"; 
-                    $pdo_izraz = $this->dbh->prepare($sql); 
-                    $kolicina= $this->getProizvodeID($id);
-                    $pdo_izraz->bindParam(':id', $id); 
-                    $smanjenaKolicina=$kolicina["kolicina"]-1;
-                    $pdo_izraz->bindParam(':kolicina', $smanjenaKolicina); 
-                    $pdo_izraz->execute(); 
-                    return true; }
-            catch(PDOException $e) 
-                    { echo "GRESKA: "; 
+                { 
+                    echo "GRESKA: "; 
                     echo $e->getMessage(); 
-                    return false; } }
+                    return false; 
+                } 
+            }
+            public function dodajProdavca($ki, $s) 
+            {   try { 
+                        $sql = "SELECT * FROM korisnik WHERE korisnicko_ime='$ki' and sifra='$s' "; 
+                        $pdo_izraz = $this->dbh->query($sql);
+                        $obj = $pdo_izraz->fetch(PDO::FETCH_ASSOC); 
+                        if($obj)
+                        {
+                            return false;
+                        }
+                        else{
+                            $sql = "INSERT INTO korisnik(korisnicko_ime,sifra,uloga) VALUES ('$ki', '$s',1)"; 
+                            $pdo_izraz = $this->dbh->exec($sql); return true;
+                            }
+                    } 
+                catch(PDOException $e) 
+                { 
+                    echo "GRESKA: "; 
+                    echo $e->getMessage(); 
+                    return false; 
+                } 
+            }
+        
+            public function izmeniKolicinu($id) 
+            {    
+                try { 
+                        $sql = "UPDATE proizvod SET kolicina=:kolicina WHERE id=:id"; 
+                        $pdo_izraz = $this->dbh->prepare($sql); 
+                        $kolicina= $this->getProizvodeID($id);
+                        $pdo_izraz->bindParam(':id', $id); 
+                        $smanjenaKolicina=$kolicina["kolicina"]-1;
+                        $pdo_izraz->bindParam(':kolicina', $smanjenaKolicina); 
+                        $pdo_izraz->execute(); 
+                        return true; 
+                    }
+                    catch(PDOException $e) 
+                    { 
+                        echo "GRESKA: "; 
+                        echo $e->getMessage(); 
+                        return false; 
+                    } 
+            }
     }
 ?>
